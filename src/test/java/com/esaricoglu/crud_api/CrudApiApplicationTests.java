0b5b800e7edbe5abe7b4660d8477bee64a3a8927
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2024-12-25 17:27:47 +0100

    Initial commit

---
+++ src/test/java/com/esaricoglu/crud_api/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esaricoglu.crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
