378ebb1ee576cf09a6652862db546fe2b26f2f05
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2024-12-25 17:53:43 +0100

    Implemented UserDetails
    Implemented UserDetails at User entity class

--- src/main/java/com/esaricoglu/model/User.java
+++ src/main/java/com/esaricoglu/model/User.java
@@ -1,10 +1,15 @@
 package com.esaricoglu.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +20,11 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
     public User(){}
 
