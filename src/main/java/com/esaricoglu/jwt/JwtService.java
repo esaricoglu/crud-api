71fd65854c6757065ba4c75979c9e0adc58cc0ff
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2024-12-25 18:17:26 +0100

    Created JwtService

---
+++ src/main/java/com/esaricoglu/jwt/JwtService.java
@@ -0,0 +1,52 @@
+package com.esaricoglu.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+    private static final String SECRET_KEY = "cIuFNAM2Jgu/eTZHfRWrzIq35lENFDMGYPgmfBP8SYQ=";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public <T> T exportToken(String token, Function<Claims, T> claimsTFunction) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token).getBody();
+
+        return claimsTFunction.apply(claims);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return exportToken(token, Claims::getSubject);
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expiryDate = exportToken(token, Claims::getExpiration);
+        return new Date().before(expiryDate);
+    }
+
+    public Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
