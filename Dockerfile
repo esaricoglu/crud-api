ef7f91693dbfcc061d396ebde03b9dbe213e951c
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2024-12-30 01:48:47 +0100

    Updated Docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17-jdk-slim
+
+ARG JAR_FILE=target/crud-api-0.0.1-SNAPSHOT.jar
+
+COPY ${JAR_FILE} application.jar
+
+ENTRYPOINT ["java", "-jar", "/application.jar"]
--- src/test/java/com/esaricoglu/starter/CrudApiApplicationTests.java
+++ src/test/java/com/esaricoglu/starter/CrudApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.esaricoglu.starter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=CrudApiApplication.class)
 class CrudApiApplicationTests {
 
 	@Test
